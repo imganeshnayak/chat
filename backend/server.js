1f3887e3a6aba310e1c292f624f1338c3a239446
Ganesh Nayak <ganesh@realmeta.ca>
2026-03-01 13:01:17 +0530

    hardcode krovaa.com in CORS allowed origins

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,9 @@ const allowedOrigins = [
     'http://localhost:8080',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:8080',
+    'https://krovaa.com',
+    'https://www.krovaa.com',
+    'http://krovaa.com',
     process.env.FRONTEND_URL,
     process.env.FRONTEND_URL?.toLowerCase()
 ].filter(Boolean);
